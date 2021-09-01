c1455707892b7de7f48009a608264140f1de6a79
kabagema <35719135+kabagema@users.noreply.github.com>
2021-09-01 18:02:42 +0200

    checkpoint

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,21 +1,23 @@
 import { BUILDER_KEYS, tSTypeAliasDeclaration } from '@babel/types';
 import * as React from 'react';
-import { Image, Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import { StyleSheet } from 'react-native';
 
 import ChatRoomItem from '../components/ChatRoomItem';
 
-import chatRoomData from '../assets/dummy-data/ChatRooms';
+import chatRoomsData from '../assets/dummy-data/ChatRooms';
 
-const chatRoom1 = chatRoomData[0];
-const chatRoom2 = chatRoomData[1];
 
 export default function TabOneScreen() {
 
   return (
       <View style={styles.page}>
-      <ChatRoomItem  chatRoom={chatRoom1}/>
-      <ChatRoomItem  chatRoom={chatRoom2}/>
+        <FlatList
+          data={chatRoomsData}
+          renderItem={({item}) => <ChatRoomItem  chatRoom={item}/>}
+          showsVerticalScrollIndicator={false}
+
+        />
       </View>
   );
 }
